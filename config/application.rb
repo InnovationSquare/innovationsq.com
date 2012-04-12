a28b0523b80821703dfff0584be47099cc8af51e
Caleb Wright <caleb.wright@gmail.com>
2012-04-11 21:22:37 -0400

    add landing.css to precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module InnovationsqCom
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w[landing.css]
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
